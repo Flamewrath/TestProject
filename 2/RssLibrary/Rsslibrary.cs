e7aa45d5ccf9398741331230d19405420d7304b3
Flamewrath <pi@raspberrypi>
2021-11-03 15:06:18 +0700

    Initial commit

---
+++ 2/RssLibrary/Rsslibrary.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using System.Net;
+using System.Xml;
+using System.Collections.Generic;
+using System;
+
+namespace RssLibrary
+{
+    public static class Rss
+    {
+        public class News
+        {
+            public News(string titleIn, string descrIn, string urlFileIn,
+                int newsNum)
+            {
+                Title = titleIn;
+                Descr = descrIn;
+                UrlFile = urlFileIn;
+                using (var client = new WebClient())
+                {
+                    Picture = client.DownloadData(UrlFile);
+                }
+                GroupTitle = $"Новость {newsNum + 1}";
+                Id = Guid.NewGuid();
+                // из-за отсутствия Bitmap FileStream в .NET 5.0, воспользуемся guid, чтоб не лочить файл
+                PicturePath = Path.GetTempPath() + Id;
+
+                File.WriteAllBytes(PicturePath, Picture);
+            }
+            //
+            public Guid Id { get; }
+            // Заголоовок статьи
+            public string Title { get; }
+            // Название для групбокса
+            public string GroupTitle { get; }
+            // Первый абзац статьи
+            public string Descr { get; }
+            // Ссылка на файл
+            public string UrlFile { get; }
+            // Картинка статьи (в массиве байтов)
+            public byte[] Picture { get; }
+            // Путь к картинке во временной папке
+            public string PicturePath { get; }
+
+            [Obsolete]
+            public string SendCliNewsData()
+            {
+                string pictureData = String.Empty;
+                for (int i = 0; i < Picture.Length; i++)
+                {
+                    pictureData += Picture[i].ToString();
+                }
+                return Title + ". " + Descr + " " +
+                    UrlFile + ":" + Picture;
+            }
+        }
+        public static List<News> GetNews()
+        {
+            List<News> news = new();
+            string temp = Path.GetTempPath() + "rss.xml";
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://lenta.ru/rss", temp);
+            }
+            XmlDocument doc = new();
+            doc.Load(temp);
+            XmlNodeList nodeList = doc.SelectNodes("/rss/channel/item");
+            for (int i = 0; i < 3; i++)
+            {
+                news.Add(new News(nodeList[i].SelectSingleNode("title").InnerText,
+                    nodeList[i].SelectSingleNode("description").InnerText,
+                    nodeList[i].SelectSingleNode("enclosure").Attributes["url"].Value,
+                    i));
+            }
+            return news;
+        }
+    }
+}
---
+++ 2/RssWindows/RssWindows/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows;
+using RssLibrary;
+using System;
+
+namespace RssWindows
+{
+    public partial class MainWindow : Window
+    {
+        private List<Rss.News> news = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            RefreshNews();
+        }
+
+        private void RefreshNews()
+        {
+            news = Rss.GetNews();
+            NewsIC.ItemsSource = news;
+        }
+
+        private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            NewsIC.ItemsSource = null;
+            news.Clear();
+            RefreshNews();
+        }
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void UploadToDatabase_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(Properties.Resources.SqlConnectionString);
+            SqlCommand cmd = new();
+            try
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = @"INSERT INTO RssNews VALUES (@id, @title, @descr, @pict, @modified)";
+                cmd.Connection.Open();
+                foreach (Rss.News n in news)
+                {
+                    cmd.Parameters.Add("id", System.Data.SqlDbType.UniqueIdentifier).Value = n.Id;
+                    cmd.Parameters.Add("title", System.Data.SqlDbType.VarChar).Value = n.Title;
+                    cmd.Parameters.Add("descr", System.Data.SqlDbType.VarChar).Value = n.Descr;
+                    cmd.Parameters.Add("pict", System.Data.SqlDbType.VarBinary).Value = n.Picture;
+                    cmd.Parameters.Add("modified", System.Data.SqlDbType.DateTime).Value = DateTime.Now;
+                    cmd.ExecuteNonQuery();
+                    cmd.Parameters.Clear();
+                }
+                MessageBox.Show("Данные сохранены!");
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении данных в БД: {ex.Message}");
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+        }
+    }
+}
